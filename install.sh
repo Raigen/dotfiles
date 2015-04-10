68ed1321519cb4c0b237408ece0f81425da26de6
Florian Oellerich <f.oellerich@epages.com>
2015-04-10 16:27:02 +0200

    feat(backup): backup `.bashrc` and `.gitconfig`
    backup both files when there is already such a file in $HOME

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -f ~/.bashrc ]
+then
+    mv ~/.bashrc ~/.bashrc.backup
+fi
+
+if [ -f ~/.gitconfig ]
+then
+    mv ~/.gitconfig ~/.gitconfig.backup
+fi
+
 ln -s ~/bin/dotfiles/.bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/bin/dotfiles/.gitignore ~/.gitignore
+
